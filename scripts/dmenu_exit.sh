18b2037b74a960f32dd23bc9366719f0f584261f
macarrie <mathieu0carrie@gmail.com>
2016-03-26 23:40:38 +0100

    Nouveau exit menu via dmenu

---
+++ scripts/dmenu_exit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Show session menu
+
+DMENU="dmenu -i -nb $1 -nf #eeeeee -sb $1 -sf $2"
+
+choice=$(echo -e "Logout\nHibernate\nReboot\nShutdown" | $DMENU)
+
+case "$choice" in
+    Logout) i3-msg exit & ;;
+    Hibernate) xautolock -locknow && systemctl hibernate -i & ;;
+    Reboot) systemctl reboot & ;;
+    Shutdown) systemctl poweroff -i & ;;
+esac
