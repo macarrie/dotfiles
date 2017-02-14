75aecf7056d246cbc5454a3e4eb5bfd6124323a3
macarrie <mathieu0carrie@gmail.com>
2017-02-14 16:24:07 +0100

    Shutdown renommé en Poweroff

--- files/scripts/dmenu_exit.sh
+++ files/scripts/dmenu_exit.sh
@@ -5,11 +5,11 @@
 #DMENU="dmenu -i -nb $1 -nf #eeeeee -sb $1 -sf $2"
 DMENU="rofi -i -dmenu"
 
-choice=$(echo -e "Logout\nHibernate\nReboot\nShutdown" | $DMENU)
+choice=$(echo -e "Logout\nHibernate\nReboot\nPoweroff" | $DMENU)
 
 case "$choice" in
     Logout) i3-msg exit & ;;
     Hibernate) xautolock -locknow && systemctl hibernate -i & ;;
     Reboot) systemctl reboot & ;;
-    Shutdown) systemctl poweroff -i & ;;
+    Poweroff) systemctl poweroff -i & ;;
 esac
